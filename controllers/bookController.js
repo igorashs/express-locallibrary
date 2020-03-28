f3baeec7d13c377324e52f8bd981af981346c9fc
igorashs <igorashsirbu@gmail.com>
2020-03-28 20:24:49 +0200

    get counts from db

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,7 +1,32 @@
 const Book = require('../models/book');
+const Author = require('../models/author');
+const Genre = require('../models/genre');
+const BookInstance = require('../models/bookinstance');
 
-exports.index = function(req, res) {
-  res.send('NOT IMPLEMENTED: Site Home Page');
+exports.index = async function(req, res) {
+  let counts = {};
+  const error = null;
+
+  try {
+    const book_count = Book.countDocuments({});
+    const book_instance_count = BookInstance.countDocuments({});
+    const book_instance_available_count = BookInstance.countDocuments({
+      status: 'Available'
+    });
+    const author_count = Author.countDocuments({});
+    const genre_count = Genre.countDocuments({});
+
+    counts.book_count = await book_count;
+    counts.book_instance_count = await book_instance_count;
+    counts.book_instance_available_count = await book_instance_available_count;
+    counts.author_count = await author_count;
+    counts.genre_count = await genre_count;
+  } catch (err) {
+    console.log(err);
+    error = err;
+  } finally {
+    res.render('index', { title: 'Local Library Home', error, data: counts });
+  }
 };
 
 // Display list of all books
