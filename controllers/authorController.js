42c0fad31149342eb764d9a470ef242a7995a50d
igorashs <igorashsirbu@gmail.com>
2020-04-03 20:53:55 +0300

    update author_update handlers

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,6 +1,7 @@
 const Author = require('../models/author');
 const Book = require('../models/book');
-const validator = require('express-validator');
+const { body, validationResult } = require('express-validator');
+const moment = require('moment');
 
 // display list of all authors
 exports.author_list = function(req, res, next) {
@@ -51,37 +52,40 @@ exports.author_create_get = function(req, res) {
 
 // Handle author create on POST
 exports.author_create_post = [
-  validator
-    .body('first_name')
+  body('first_name')
     .isLength({ min: 1 })
     .trim()
     .withMessage('First name must be specified.')
     .isAlphanumeric()
     .withMessage('First name has non-alphanumeric characters.')
     .escape(),
-  validator
-    .body('family_name')
+  body('family_name')
     .isLength({ min: 1 })
     .trim()
     .withMessage('Family name must be specified')
     .isAlphanumeric()
     .withMessage('Family name has non-alphanumeric characters.')
     .escape(),
-  validator
-    .body('date_of_birth', 'Invalid date of birth')
+  body('date_of_birth', 'Invalid date of birth')
     .optional({ checkFalsy: true })
     .isISO8601()
     .toDate(),
-  validator
-    .body('date_of_death', 'Invalid date of death')
+  body('date_of_death', 'Invalid date of death')
     .optional({ checkFalsy: true })
     .isISO8601()
     .toDate(),
 
   (req, res, next) => {
-    const errors = validator.validationResult(req);
+    const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
+      req.body.date_of_birth = moment(new Date(req.body.date_of_birth)).format(
+        'YYYY-MM-DD'
+      );
+      req.body.date_of_death = moment(new Date(req.body.date_of_death)).format(
+        'YYYY-MM-DD'
+      );
+
       res.render('author_form', {
         title: 'Create Author',
         author: req.body,
@@ -144,11 +148,73 @@ exports.author_delete_post = async function(req, res, next) {
 };
 
 // Display author update form on GET
-exports.author_update_get = function(req, res) {
-  res.send('NOT IMPLEMENTED: Author update GET');
+exports.author_update_get = async function(req, res, next) {
+  try {
+    const author = await Author.findById(req.params.id);
+
+    if (author === null) {
+      const err = new Error('Author not found');
+      err.status = 404;
+      return next(err);
+    }
+
+    res.render('author_form', { title: 'Update Author', author });
+  } catch (err) {
+    return next(err);
+  }
 };
 
 // Handle author update on POST
-exports.author_update_post = function(req, res) {
-  res.send('NOT IMPLEMENTED: Author update POST');
-};
+exports.author_update_post = [
+  body('first_name')
+    .isLength({ min: 1 })
+    .trim()
+    .withMessage('First name must be specified.')
+    .isAlphanumeric()
+    .withMessage('First name has non-alphanumeric characters.')
+    .escape(),
+  body('family_name')
+    .isLength({ min: 1 })
+    .trim()
+    .withMessage('Family name must be specified')
+    .isAlphanumeric()
+    .withMessage('Family name has non-alphanumeric characters.')
+    .escape(),
+  body('date_of_birth', 'Invalid date of birth')
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+  body('date_of_death', 'Invalid date of death')
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      req.body.date_of_birth = moment(new Date(req.body.date_of_birth)).format(
+        'YYYY-MM-DD'
+      );
+      req.body.date_of_death = moment(new Date(req.body.date_of_death)).format(
+        'YYYY-MM-DD'
+      );
+
+      res.render('author_form', {
+        title: 'Update Author',
+        author: req.body,
+        errors: errors.array()
+      });
+    } else {
+      const author = new Author({ ...req.body, _id: req.params.id });
+
+      Author.findByIdAndUpdate(req.params.id, author, {}, (err, theauthor) => {
+        if (err) {
+          return next(err);
+        }
+
+        res.redirect(theauthor.url);
+      });
+    }
+  }
+];
